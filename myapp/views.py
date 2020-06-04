f040247a1f0c211e8bbf944b82a9919e83c58a2d
udasitharani <udasitharani@gmail.com>
2020-06-04 19:05:16 +0530

    Do Frontend

--- myapp/views.py
+++ myapp/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'base.html')
+
+
+def new_search(request):
+    search = request.POST.get('search')
+    data_for_frontend = {
+        'search': search,
+    }
+    return render(request, 'myapp/new_search.html', context=data_for_frontend)
